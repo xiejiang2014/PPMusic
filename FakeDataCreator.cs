83da05aebf641d496d8c26f9d1aaf522a8629b8b
xiejiang2014 <44390984+xiejiang2014@users.noreply.github.com>
2021-06-28 21:45:31 +0800

    增加素材

--- FakeDataCreator.cs
+++ FakeDataCreator.cs
@@ -213,6 +213,110 @@ namespace PPMusic
                              Description   = "美丽的相聚如期而至，那隽永的抒情挂在疲备的吉他上，往事成了一首悠远的歌，总在有夕阳的日子里浮现。我有一个小秘密，藏在我的吉他里，跟着我的拍子，唱我的旋律，让我轻轻地告诉你…",
                              Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
                          };
+            yield return new Album()
+                         {
+                             Title         = "纯音·水下光年·自然原韵",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\3179735066.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "水下光年 聆听 自然的呓语",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "纯音乐小集「山」主题",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\3244804514.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "最近总是繁忙，觉得越在城市跑越不开心，人与事人与人。终于有一晚睡觉，梦里的自己在爬山，很美很美。之后就搜索各种山的图片，听有关山的音乐，幻想自己在旅行，心情就好点了。生活总有各种无可奈何，但我们还有音乐陪伴，很好了不是吗？",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "纯音：悠然自得 聆听陶笛清脆悦耳",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\3545485941.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "陶笛（英语：Ocarina，也译作奥卡利那笛、洋埙、瓦埙、土笛、鼓浪笛等），目前国际上比较流行的陶笛多数是一种源自意大利、状似潜艇、有哨口、通常用陶土烧制的吹管乐器。               虽说世事沧桑、风雨更替，但花开不止一季，美丽的风景一直在路上。错过了春天的百花风情，我们还会迎来夏天的蛙叫蝉鸣;错过了秋天的落叶静美，我们还会迎来冬天的雪花摇曳。只要我们守住一颗宁静的心，我们就可以在滚滚红尘中，悠然自得地听高山流水、弄花香满衣。",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "犹如天使的治愈小提琴",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\3476526085.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "节奏舒服的小提琴  完美的体现了拉好一个“二胡”的重要性！",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "4500高冷海拔之地！空灵的入耳藏语歌",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\1777789617.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "以前听惯了草原，卓玛，雪山，还有姑娘……现在藏语有了流行歌曲，有了说唱，逐渐走出了自己的风格。作为一个汉族人，真心觉得好听！",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "Billboard 2018百大电音舞曲",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\5847280921.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "Billboard 2018电音舞曲榜全球百大金曲！",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "reputation巡演 | 惊喜曲目合辑",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\4368460036.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "巡演已于2018年11月21日结束。也非常期待TS7和她的新巡演。现场表演均为不插电版，可站内并没此版本，所以罗列的是已收录专辑的版本，若想看现场请前往某博关键字搜索，谢谢。",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "哇！通宵无需可乐或咖啡",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\2002998660.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "哇！通宵无需可乐或咖啡 咖啡被高考无效化了，来英国后论文，report又把可乐秒杀了，熬夜上阵顶不住怎么破～戳进来听听歌呗～ “点点，你什么时候会打推销广告了？” “这叫自荐小标语，不要把点点完美的文字不着痕迹地贬低好嘛！” “你自己通宵就算了，别祸国殃民。” “这叫助人为乐，你不懂，对于有责任感不干完活不睡觉的家伙们，点点的歌单是福音好不好！” 听完后—— “点点，你是不是那几次presentation之前都听了这该死的歌单？” “然也～” “怪不得上台和哈士奇一样兴奋，果然该给你肖像绘里加一个肥绒大尾巴。” “改我肖像立绘的时候温柔点，太重的尾巴点点拖不动” “不拖，以后天天朝我摆摆尾巴就好。” “你有毒＝＝” 事后， “点点，你是不是把我这大触的出场写的太平乏了？” “少罗嗦，点点这是在弘扬实事求是的中华传统美德，乖乖赶稿～” “嗯，我去把你黑眼圈加进立绘里。” “！！！”",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "势不可挡！2017混音大佬冲榜力作！",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\878028874.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "每首都经过精挑细选，还有两个月就该评选今年的全球百大DJ",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "出神电音区，够胆你就进 ▪ Trance",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\4237687835.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "关于Trance，大部分人知道”出神“一词，早期Trance为techno和house的混合体，它融合了大量的techno的拍子和节奏方面的结构，但同时加入了更多旋律优美的段子。同时，在鼓点结构方面不像house那样富有活力和拥有难以预测的提升效果。这些trance形式被定义为古典trance比起后辈那些更能带动舞池气氛的trance来说要冗长和抽象。",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "下班最来劲",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\5109267378.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "下班倒计时五分钟，兴奋情绪飙升中。努力工作明天再说，现在只想听一首歌，放飞自我，让你下班最来劲。每月不定期更新～",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "如斯女嗓 骨子里的高冷",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\2914617281.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "▼ 姑娘 你如此出众 你的气质诱我至深 让我狠狠拥抱你的清冷 觊觎你骨子里的莫测 无谓曲高和寡 无关世间万千 你本高冷 我本钟情 5.18.2016♥",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
+            yield return new Album()
+                         {
+                             Title         = "快节奏 亲密的音乐告白",
+                             CoverImageUrl = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources\\Images\\Covers\\256\\3527197788.jpg"),
+                             PlayCountText = "3.5万",
+                             Description   = "音乐的旋律是委婉唯美的诗歌 是奔放狂野的气息  是迷离徜恍的梦境  是我们内心深处的呐喊 也是心灵的亲密告白：        喜欢文字 喜欢音乐 喜欢绘画 喜欢舞蹈 喜欢精美细腻的小礼物 那么多那么多的喜欢 不及对你的喜欢 喜欢天马行空的想象 却不及想念你的万分之一             幻想如此美妙 没有你心中空洞的无法自我疗伤  唯有在睡梦中望着你深邃的目光 黎明到来也不能遗忘 ……",
+                             Songs         = new ObservableCollection<Song>(GetRandomSongs(3))
+                         };
         }
 
 
